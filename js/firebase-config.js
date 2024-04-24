aaf4d46db859decc90e114715e26ea1b4a53ff0d
Luciano Silva <luciano_jet@hotmail.com>
2024-04-24 11:55:15 -0300

    criado js do firebase-config

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQGVTy0cZXc4HO3MzO4_S6mtMBdhbCzVE",
+  authDomain: "site-monhang.firebaseapp.com",
+  databaseURL: "https://site-monhang-default-rtdb.firebaseio.com",
+  projectId: "site-monhang",
+  storageBucket: "site-monhang.appspot.com",
+  messagingSenderId: "627165670566",
+  appId: "1:627165670566:web:3477f3251f771b9311d867"
+};
+
+
+
+console.log("Funciona!!!!!")
+
+
+const app = initializeApp(firebaseConfig);
